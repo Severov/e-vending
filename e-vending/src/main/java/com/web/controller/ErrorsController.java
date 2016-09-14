351a98b8c6c1f7bf28285ac186e3b0d3d2e12145
mishka <mishka@my-debian-home>
2016-09-14 06:42:18 +0300

    error 404

---
+++ e-vending/src/main/java/com/web/controller/ErrorsController.java
@@ -0,0 +1,40 @@
+package com.web.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorsController {
+
+	@RequestMapping(value = "/404", method = RequestMethod.GET)
+	public String error404() {
+		return "errors/404";
+	}
+
+	// for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
+
+		ModelAndView model = new ModelAndView();
+
+		// check if user is login
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			UserDetails userDetail = (UserDetails) auth.getPrincipal();
+			System.out.println(userDetail);
+
+			model.addObject("username", userDetail.getUsername());
+		}
+
+		model.setViewName("errors/403");
+		return model;
+
+	}
+
+}
--- e-vending/src/main/java/com/web/controller/MainController.java
+++ e-vending/src/main/java/com/web/controller/MainController.java
@@ -3,7 +3,6 @@ package com.web.controller;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,11 +34,6 @@ public class MainController {
 		return "private/main";
 	}
 
-	@RequestMapping(value = "/404", method = RequestMethod.GET)
-	public String error404() {
-		return "errors/404";
-	}
-
 	@RequestMapping(value = "/private/admin", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
 		ModelAndView model = new ModelAndView();
@@ -48,26 +42,6 @@ public class MainController {
 		model.setViewName("private/admin");
 
 		return model;
-	}
-
-	// for 403 access denied page
-	@RequestMapping(value = "/403", method = RequestMethod.GET)
-	public ModelAndView accesssDenied() {
-
-		ModelAndView model = new ModelAndView();
-
-		// check if user is login
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (!(auth instanceof AnonymousAuthenticationToken)) {
-			UserDetails userDetail = (UserDetails) auth.getPrincipal();
-			System.out.println(userDetail);
-
-			model.addObject("username", userDetail.getUsername());
-		}
-
-		model.setViewName("errors/403");
-		return model;
-
 	}
 
 }
